5f214e43c41daeb19c0ca8d6699fd74e1884b4c6
young219257 <young219257@gmail.com>
2024-08-22 01:35:17 +0900

    [#1] MailConfig 추가

---
+++ src/main/java/com/sparta/firstseversystem/global/config/MailConfig.java
@@ -0,0 +1,50 @@
+package com.sparta.firstseversystem.global.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+@PropertySource("classpath:mailAuth.properties")
+@ConfigurationProperties(prefix = "mail")
+@Getter
+@Setter
+@ToString
+public class MailConfig {
+
+    private String username;
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.naver.com");
+        mailSender.setPort(465);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        mailSender.setJavaMailProperties(getMailProperties()); // 메일 인증서버 정보 가져오기
+
+        return mailSender;
+    }
+
+    private Properties getMailProperties() {
+        Properties properties = new Properties();
+        properties.setProperty("mail.transport.protocol", "smtp"); // 프로토콜 설정
+        properties.setProperty("mail.smtp.auth", "true"); // smtp 인증
+        properties.setProperty("mail.smtp.starttls.enable", "true"); // smtp strattles 사용
+        properties.setProperty("mail.debug", "true"); // 디버그 사용
+        properties.setProperty("mail.smtp.ssl.trust","smtp.naver.com"); // ssl 인증 서버는 smtp.naver.com
+        properties.setProperty("mail.smtp.ssl.enable","true"); // ssl 사용
+        return properties;
+    }
+
+}
