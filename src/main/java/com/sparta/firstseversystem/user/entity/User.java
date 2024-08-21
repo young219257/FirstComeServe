1d262c2284fe8efea171d6a801133ec4a6afb34f
young219257 <young219257@gmail.com>
2024-08-21 15:14:47 +0900

    init commit

---
+++ src/main/java/com/sparta/firstseversystem/user/entity/User.java
@@ -0,0 +1,33 @@
+package com.sparta.firstseversystem.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+
+@Entity
+@Getter
+@Table(name="users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String phoneNumber;
+
+    @Column(nullable = false)
+    private String address;
+
+
+
+}
---
+++ src/test/java/com/sparta/firstseversystem/FirstSeverSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.firstseversystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSeverSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
