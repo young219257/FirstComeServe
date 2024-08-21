e163b8d494a862e94e28c70617488ff0da35deb8
young219257 <young219257@gmail.com>
2024-08-21 16:15:59 +0900

    git commit -m "Remove cached files from tracking"

---
+++ src/main/java/com/sparta/firstseversystem/global/security/filter/AuthenticationFilter.java
@@ -0,0 +1,19 @@
+package com.sparta.firstseversystem.global.security.filter;
+
+
+import com.sparta.firstseversystem.global.security.utils.JwtUtils;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Slf4j(topic = "로그인 및 JWT 생성")
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter{
+    private final JwtUtils jwtUtils;
+
+
+    public AuthenticationFilter(JwtUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+        setFilterProcessesUrl("/api/members/login");
+    }
+}
---
+++ src/main/java/com/sparta/firstseversystem/global/security/service/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.sparta.firstseversystem.global.security.service;
+
+import com.sparta.firstseversystem.user.entity.User;
+import com.sparta.firstseversystem.user.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new CustomUserDetails(user);
+    }
+}
