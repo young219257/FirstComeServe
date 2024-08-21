7efef855ab88edddd38989812c909d4729d6ce41
young219257 <young219257@gmail.com>
2024-08-21 16:15:05 +0900

    git commit -m "Remove cached files from tracking"

--- src/main/java/com/sparta/firstseversystem/user/controller/UserController.java
+++
@@ -1,8 +0,0 @@
-package com.sparta.firstseversystem.user.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class UserController {
-
-}
--- src/main/java/com/sparta/firstseversystem/user/service/UserService.java
+++
@@ -1,4 +0,0 @@
-package com.sparta.firstseversystem.user.service;
-
-public interface UserService {
-}
