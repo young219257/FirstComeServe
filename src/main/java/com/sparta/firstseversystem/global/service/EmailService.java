e5918b7739fdc15d87498893919ec1ba56ee1131
young219257 <young219257@gmail.com>
2024-08-22 14:58:51 +0900

    [#1] Email 인증 관련 메소드 생성

---
+++ src/main/java/com/sparta/firstseversystem/global/service/EmailService.java
@@ -0,0 +1,17 @@
+package com.sparta.firstseversystem.global.service;
+
+
+import jakarta.mail.internet.MimeMessage;
+
+public interface EmailService {
+    public String createToken();
+
+    //이메일 형식 메서드
+    MimeMessage createMimeMessage(String email, String token);
+
+    //이메일 전송 메서드
+    void sendEmail(String toEmail);
+
+    //이메일 토큰 가져오는 메소드
+    String getEmailByToken(String token);
+}
