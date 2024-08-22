528817a9c77eadc5e8dcb62fd4fac9bf6650e8f6
young219257 <young219257@gmail.com>
2024-08-22 15:02:32 +0900

    [#2] 유저 관리(회원가입, 이메일 인증 관련 기능) 추가

--- src/main/java/com/sparta/firstseversystem/user/controller/UserController.java
+++ src/main/java/com/sparta/firstseversystem/user/controller/UserController.java
@@ -1,8 +1,54 @@
 package com.sparta.firstseversystem.user.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.sparta.firstseversystem.global.exception.handler.dto.ApiResponse;
+import com.sparta.firstseversystem.user.dto.SignupDto;
+import com.sparta.firstseversystem.user.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
+
+
+    //회원가입 api
+    @PostMapping("/signup")
+    public ApiResponse signup(@RequestBody SignupDto requestDto) {
+        userService.signup(requestDto);
+        return ApiResponse.ok(200,"회원가입 성공");
+
+    }
+
+    //이메일 인증 완료 시 브라우저에서 서버로 get요청
+    @GetMapping("/varify")
+    public ApiResponse varifyEmail(@RequestParam("token") String token) {
+        boolean isVerified = userService.verifyEmail(token);
+        if (isVerified) {
+            return ApiResponse.ok(200,"이메일 인증이 완료되었습니다.");
+        }
+        else{
+            return ApiResponse.error(400,"유효하지 않은 토큰입니다.");
+        }
+    }
+
+    /**로그인 api
+     @Param : email, password
+     @return : 성공 여부
+     **/
+
+
+    /**로그아웃 api
+     @Param : userId
+     @return : 성공 여부
+     **/
+
+    /**회원 수정 api
+     method : put
+     @Param : userId
+     @return : 성공 여부
+     **/
 
 }
--- src/main/java/com/sparta/firstseversystem/user/entity/User.java
+++ src/main/java/com/sparta/firstseversystem/user/entity/User.java
@@ -1,12 +1,21 @@
 package com.sparta.firstseversystem.user.entity;
 
+import com.sparta.firstseversystem.user.dto.SignupDto;
+import com.sparta.firstseversystem.user.utils.EncryptionUtils;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 
 @Entity
 @Getter
 @Table(name="users")
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
 
     @Id
@@ -28,6 +37,25 @@ public class User {
     @Column(nullable = false)
     private String address;
 
+    @Column(nullable = false)
+    private boolean emailVarified;
 
 
+    public static User of(SignupDto requestDto, BCryptPasswordEncoder encoder){
+
+        //비밀번호 : 단방향 암호화
+        //그 외 개인 정보 : 양방향 암호화
+        return User.builder()
+                .username(EncryptionUtils.encrypt(requestDto.getName()))
+                .email(EncryptionUtils.encrypt(requestDto.getEmail()))
+                .password(encoder.encode(requestDto.getPassword()))
+                .address(EncryptionUtils.encrypt(requestDto.getAddress()))
+                .phoneNumber(EncryptionUtils.encrypt(requestDto.getPhoneNumber()))
+                .emailVarified(false)
+                .build();
+    }
+
+    public void setEmailVerified(boolean b) {
+        this.emailVarified = b;
+    }
 }
