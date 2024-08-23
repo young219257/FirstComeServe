a3befdf9841b22ae88027b5c31522f91079b6568
young219257 <young219257@gmail.com>
2024-08-23 22:19:51 +0900

    [#7] order 연관관계 추가

--- src/main/java/com/sparta/firstseversystem/domain/delivery/entity/Delivery.java
+++ src/main/java/com/sparta/firstseversystem/domain/delivery/entity/Delivery.java
@@ -1,7 +1,8 @@
 package com.sparta.firstseversystem.domain.delivery.entity;
 
 
-import com.sparta.firstseversystem.global.entity.TimeStamped;
+import com.sparta.firstseversystem.domain.delivery.type.DeliveryStatus;
+import com.sparta.firstseversystem.domain.order.entity.Order;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -28,6 +29,7 @@ public class Delivery {
     private String phoneNumber;
 
     @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
     private DeliveryStatus deliveryStatus;
 
     @Column(nullable = false)
@@ -35,4 +37,8 @@ public class Delivery {
 
     @Column(nullable = false)
     private Timestamp completedAt;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="order_id",nullable = false)
+    private Order order;
 }
