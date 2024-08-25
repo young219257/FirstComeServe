138fef20f86dcc9f2402b15e4505dae1f75ce5b9
young219257 <young219257@gmail.com>
2024-08-25 21:20:41 +0900

    [#5] 주석 추가

--- src/main/java/com/sparta/firstseversystem/domain/product/controller/ProductController.java
+++ src/main/java/com/sparta/firstseversystem/domain/product/controller/ProductController.java
@@ -17,6 +17,7 @@ public class ProductController {
 
     private final ProductService productService;
 
+    /**상품 목록 조회**/
     @GetMapping
     public ApiResponse<Page<ProductListResponseDto>> getAllProducts(@RequestParam(value = "page", defaultValue = "0") int page,
                                                                     @RequestParam(value = "size", defaultValue = "10") int size,
@@ -25,6 +26,8 @@ public class ProductController {
         Page<ProductListResponseDto> products=productService.getAllProducts(page, size, sortBy, isAsc);
         return ApiResponse.ok(HttpStatus.OK.value(), "상품 목록 조회에 성공하셨습니다.", products);
     }
+
+    /**상품 상세 조회**/
     @GetMapping("/{productId}")
     public ApiResponse<ProductResponseDto> getProductById(@PathVariable("productId") Long productId) {
         ProductResponseDto product=productService.getProduct(productId);
