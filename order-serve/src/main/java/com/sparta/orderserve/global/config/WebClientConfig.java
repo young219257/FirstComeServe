0d1238dbbfd7b4a3315d321f9885c610624e6e4d
young219257 <young219257@gmail.com>
2024-08-28 22:35:41 +0900

    [#16] webCient 설정

---
+++ order-serve/src/main/java/com/sparta/orderserve/global/config/WebClientConfig.java
@@ -0,0 +1,20 @@
+package com.sparta.orderserve.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("http://order-service:8080/api/user")
+                .defaultHeaders(httpHeaders -> {
+                    httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+                    httpHeaders.add("apiKey","API Key 값 입력");
+                })
+                .build();
+    }
+}
