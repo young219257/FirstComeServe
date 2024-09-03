fcbe21583cca5c4d43c7692dbc5aa17f043502b1
young219257 <young219257@gmail.com>
2024-09-03 10:21:59 +0900

    [#16] ApiResponse 기본 생성자 추가

--- order-serve/src/main/java/com/sparta/orderserve/global/exception/handler/dto/ApiResponse.java
+++ order-serve/src/main/java/com/sparta/orderserve/global/exception/handler/dto/ApiResponse.java
@@ -3,9 +3,11 @@ package com.sparta.orderserve.global.exception.handler.dto;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @AllArgsConstructor
 @Getter
+@NoArgsConstructor
 public class ApiResponse<T> {
     int statusCode;
     String message;
