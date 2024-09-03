93c7f4debc1610ae02e1e86f76d4464b38488256
young219257 <young219257@gmail.com>
2024-09-03 10:35:50 +0900

    [#16] orderClient 메서드 return 타입 변경

--- order-serve/src/main/java/com/sparta/orderserve/domain/order/client/OrderClient.java
+++ order-serve/src/main/java/com/sparta/orderserve/domain/order/client/OrderClient.java
@@ -4,40 +4,45 @@ package com.sparta.orderserve.domain.order.client;
 import com.sparta.orderserve.domain.order.dto.ProductDto;
 import com.sparta.orderserve.domain.order.dto.ProductUpdateRequestDto;
 import com.sparta.orderserve.domain.order.dto.UserDto;
+import com.sparta.orderserve.global.exception.handler.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
+import java.lang.reflect.Type;
+
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class OrderClient {
 
     private final WebClient webClient;
 
 
     //user 정보 가져오는 메소드(webClient)
-    public  Mono<UserDto> getUserById(Long userId){
+    public  Mono<ApiResponse<UserDto>> getUserById(Long userId){
         return webClient.get()
                 .uri("http://localhost:8080/api/external/user/"+userId)
                 .retrieve()
-                .bodyToMono(UserDto.class);
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<UserDto>>() {} );
     }
 
 
-    //상품 가져오는 메소드(webClient)
-    public Mono<ProductDto> getProductById(Long productId) {
+    // 상품을 가져오는 메소드
+    public Mono<ApiResponse<ProductDto>> getProductById(Long productId) {
         return webClient.get()
-                .uri("http://localhost:8080/api/external/product/"+productId)
+                .uri("http://localhost:8082/api/external/product/{productId}", productId)
                 .retrieve()
-                .bodyToMono(ProductDto.class);
+                .bodyToMono(new ParameterizedTypeReference<ApiResponse<ProductDto>>() {});
     }
-
 
     //상품 제고 업데이트 하는 메소드
     public void updateProductStock(ProductDto productDto,int newStockQuantity){
         webClient.put().
-                uri("http://localhost:8080/product/"+productDto.getProductId())
+                uri("http://localhost:8082/api/external/product/{productId}", productDto.getProductId())
                 .bodyValue(new ProductUpdateRequestDto(newStockQuantity))
                 .retrieve()
                 .toBodilessEntity();
