27430bd197f6e863603b9c0e02d6242d96986a12
young219257 <young219257@gmail.com>
2024-09-03 10:46:33 +0900

    [#17] apigateway module init setting

---
+++ apigateway-serve/src/main/java/com/sparta/apigatewayserve/ApigatewayServeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.apigatewayserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApigatewayServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApigatewayServeApplication.class, args);
+    }
+
+}
