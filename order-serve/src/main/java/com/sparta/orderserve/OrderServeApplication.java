2bb9a94736649832d1caf780f55f30381782ac53
young219257 <young219257@gmail.com>
2024-09-09 18:25:38 +0900

    [#20] eureka client 설정

--- order-serve/src/main/java/com/sparta/orderserve/OrderServeApplication.java
+++ order-serve/src/main/java/com/sparta/orderserve/OrderServeApplication.java
@@ -2,12 +2,14 @@ package com.sparta.orderserve;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableJpaAuditing
 @EnableScheduling
+@EnableDiscoveryClient
 public class OrderServeApplication {
 
     public static void main(String[] args) {
