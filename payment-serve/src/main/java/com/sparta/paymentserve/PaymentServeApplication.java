c53242b695ca60473e55a2620ec8bce536bd3ccb
young219257 <young219257@gmail.com>
2024-09-11 16:07:04 +0900

    [#36] payment 모듈 추가

---
+++ payment-serve/src/main/java/com/sparta/paymentserve/PaymentServeApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.paymentserve;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServeApplication.class, args);
+    }
+
+}
---
+++ payment-serve/src/test/java/com/sparta/paymentserve/PaymentServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.paymentserve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentServeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
