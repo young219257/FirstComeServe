1ce27235a038a1416fc9eb176d966c470a928749
young219257 <young219257@gmail.com>
2024-09-11 16:01:44 +0900

    [#37] 남은 수량 api 추가

---
+++ product-serve/src/main/java/com/sparta/productserve/domain/product/dto/ProductStockDto.java
@@ -0,0 +1,15 @@
+package com.sparta.productserve.domain.product.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+public class ProductStockDto {
+    int stock;
+
+}
