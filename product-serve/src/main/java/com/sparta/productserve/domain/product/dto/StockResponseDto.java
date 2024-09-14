06d75068a36d670ae48ce898ab7f8adfbd71c6e6
young219257 <young219257@gmail.com>
2024-09-14 20:12:59 +0900

    [#36] rename productStockDto

--- product-serve/src/main/java/com/sparta/productserve/domain/product/dto/StockResponseDto.java
+++ product-serve/src/main/java/com/sparta/productserve/domain/product/dto/StockResponseDto.java
@@ -9,7 +9,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Getter
-public class ProductStockDto {
+public class StockResponseDto {
+    Long productId;
+    String productName;
     int stock;
-
 }
