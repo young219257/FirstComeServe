9bd8dc33074f5977e61076818868a9e65f373c75
young219257 <young219257@gmail.com>
2024-09-14 20:05:10 +0900

    [#36] 필요 없는 파일 삭제

--- payment-serve/src/main/java/com/sparta/paymentserve/global/exception/InvalidOrderStatusException.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.paymentserve.global.exception;
-
-public class InvalidOrderStatusException extends RuntimeException {
-    private final ErrorCode errorCode;
-    public InvalidOrderStatusException(ErrorCode errorCode) {
-        super(errorCode.getMessage());
-        this.errorCode = errorCode;
-    }
-}
