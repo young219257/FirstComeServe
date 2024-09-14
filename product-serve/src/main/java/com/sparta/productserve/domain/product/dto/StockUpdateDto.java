46a9f7b4f977b379315df012252cc14bcd351277
young219257 <young219257@gmail.com>
2024-09-14 20:13:24 +0900

    [#36] rename StockUpdateDto

--- product-serve/src/main/java/com/sparta/productserve/domain/product/dto/StockUpdateDto.java
+++ product-serve/src/main/java/com/sparta/productserve/domain/product/dto/StockUpdateDto.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductStockUpdateDto {
+public class StockUpdateDto {
 
     private Long productId;
     private int quantity;
