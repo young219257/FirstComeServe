4e723f3791d7b2eed68bf9db7b4b5b8c32fb9091
young219257 <young219257@gmail.com>
2025-03-23 02:44:03 +0900

    [refactor] webClient 설정 추가

--- order-serve/src/main/java/com/sparta/orderserve/global/config/WebClientConfig.java
+++ order-serve/src/main/java/com/sparta/orderserve/global/config/WebClientConfig.java
@@ -1,10 +1,17 @@
 package com.sparta.orderserve.global.config;
 
+import io.netty.channel.ChannelOption;
+import io.netty.handler.timeout.ReadTimeoutHandler;
+import io.netty.handler.timeout.WriteTimeoutHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
 
 @Configuration
 public class WebClientConfig {
@@ -16,6 +23,15 @@ public class WebClientConfig {
                 .defaultHeaders(httpHeaders -> {
                     httpHeaders.add(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
                 })
+                .clientConnector(new ReactorClientHttpConnector(
+                        HttpClient.create()
+                                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)  // 5초 내에 연결되지 않으면 실패
+                                .responseTimeout(Duration.ofSeconds(5))  // 응답 타임아웃 5초
+                                .doOnConnected(conn ->
+                                        conn.addHandlerLast(new ReadTimeoutHandler(5))
+                                                .addHandlerLast(new WriteTimeoutHandler(5))
+                                )
+                ))
                 .build();
     }
 }
